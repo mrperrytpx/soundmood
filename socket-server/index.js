809719498b5ec891a17bd356ec4b3c86433e7515
mrperrytpx <tomislav.siprak1996@gmail.com>
2023-05-06 20:52:45 +0200

    cors fix

--- socket-server/index.js
+++ socket-server/index.js
@@ -7,21 +7,16 @@ const cors = require("cors");
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
-
-const MESSAGES = Object.freeze({
-  INCREMENT: "increment",
-  DECREMENT: "decrement",
+const io = new Server(server, {
+  cors: {
+    origin: process.env.CLIENT_URL,
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
 });
 
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL,
-    credentials: true,
-  })
-);
-
 app.use(helmet());
+app.use(cors({ origin: process.env.CLIENT_URL }));
 
 io.on("connection", (socket) => {
   console.log("user connection");
@@ -30,3 +25,8 @@ io.on("connection", (socket) => {
 server.listen(process.env.PORT || 5050, () =>
   console.log(`SERVER STARTED AT PORT ${process.env.PORT}`)
 );
+
+const MESSAGES = Object.freeze({
+  INCREMENT: "increment",
+  DECREMENT: "decrement",
+});
