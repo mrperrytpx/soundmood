2646ad642852221e3b0e3cc22732efd33e15a301
mrperrytpx <tomislav.siprak1996@gmail.com>
2023-05-06 21:28:30 +0200

    scuffed setup but works

--- socket-server/index.js
+++ socket-server/index.js
@@ -18,8 +18,38 @@ const io = new Server(server, {
 app.use(helmet());
 app.use(cors({ origin: process.env.CLIENT_URL }));
 
+let visitors = 0;
+let listeners = 0;
+
 io.on("connection", (socket) => {
+  visitors++;
+
+  socket.broadcast.emit("user-connect", { visitors, listeners });
+
+  let isStreaming = false;
   console.log("user connection");
+
+  socket.on("start-streaming", () => {
+    console.log("started streaming");
+    isStreaming = true;
+    listeners++;
+    socket.broadcast.emit("start-streaming", { listeners });
+  });
+
+  socket.on("stop-streaming", () => {
+    console.log("stopped streaming");
+    isStreaming = false;
+    listeners--;
+    socket.broadcast.emit("start-streaming", { listeners });
+  });
+
+  // socket.on("visibility-change", () => {});
+
+  socket.on("disconnect", () => {
+    visitors--;
+    if (isStreaming) listeners--;
+    socket.broadcast.emit("user-disconnect", { visitors, listeners });
+  });
 });
 
 server.listen(process.env.PORT || 5050, () =>
